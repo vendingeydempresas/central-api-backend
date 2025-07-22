fcc415674106c8ec356a897359bc07c1b2df71c2
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-21 20:50:44 -0400

    Add files via upload

--- src/routes/eventos.js
+++ src/routes/eventos.js
@@ -1,1 +1,10 @@
-// eventos routes
+// routes/eventos.js
+const express = require('express');
+const router = express.Router();
+
+// Aquí tus controladores y rutas, por ejemplo:
+router.get('/', (req, res) => {
+  res.send('Lista de eventos');
+});
+
+module.exports = router;
