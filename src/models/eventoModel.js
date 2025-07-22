b0778bdfaf5adf7cee7283035a7cf4e4340e3238
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-22 19:06:29 -0400

    Add files via upload

--- src/models/eventoModel.js
+++ src/models/eventoModel.js
@@ -2,9 +2,24 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Evento = sequelize.define('Evento', {
-    tipo_evento: DataTypes.STRING,
-    detalle: DataTypes.JSON,
-    fecha: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+  tipo_evento: DataTypes.STRING,
+  ID_Poducto_IV: DataTypes.STRING,
+  Nombre_Producto_IV: DataTypes.STRING,
+  Cantidad_Link_Pago_IV: DataTypes.INTEGER,
+  Precio_Venta_IV: DataTypes.FLOAT,
+  Descripcion_IV: DataTypes.STRING,
+  Cantidad_Cargada_IV: DataTypes.INTEGER,
+  Numero_Vending_IV: DataTypes.STRING,
+  Numero_Motor_IV: DataTypes.STRING,
+  Link_Pago: DataTypes.STRING,
+  QR_Link_Pago: DataTypes.STRING,
+  Lote_Cargado_IV: DataTypes.STRING,
+  Precio_Compra_IV: DataTypes.FLOAT,
+  timestamp: DataTypes.DATE,
+  fecha: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
 });
 
 module.exports = Evento;
