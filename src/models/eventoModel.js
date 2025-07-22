cb505db07b70ec7cfa4d90e266c806c2c1252d78
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-22 19:41:53 -0400

    Add files via upload

--- src/models/eventoModel.js
+++ src/models/eventoModel.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/database');
 
 const Evento = sequelize.define('Evento', {
   tipo_evento: DataTypes.STRING,
-  ID_Poducto_IV: DataTypes.STRING,
+  ID_Producto_IV: DataTypes.STRING,
   Nombre_Producto_IV: DataTypes.STRING,
   Cantidad_Link_Pago_IV: DataTypes.INTEGER,
   Precio_Venta_IV: DataTypes.FLOAT,
