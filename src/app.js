a919fa40a81d15d2d2ce708104430f0347c6e1e7
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-21 20:58:00 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -6,10 +6,14 @@ const { sequelize } = require('./models');
 
 // const authRoutes = require('./routes/auth');
 // const clienteRoutes = require('./routes/clientes');
-  const productoRoutes = require('./routes/productos');
+const productoRoutes = require('./routes/productos');
 // const ventaRoutes = require('./routes/ventas');
-  const eventoRoutes = require('./routes/eventos');
+const eventoRoutes = require('./routes/eventos');
 const webhookRoutes = require('./routes/webhook');
+
+console.log('productoRoutes:', typeof productoRoutes);
+console.log('eventoRoutes:', typeof eventoRoutes);
+
 
 const app = express();
 app.use(cors());
@@ -17,9 +21,9 @@ app.use(bodyParser.json());
 
 // app.use('/api/auth', authRoutes);
 // app.use('/api/clientes', clienteRoutes);
- app.use('/api/productos', productoRoutes);
+app.use('/api/productos', productoRoutes);
 // app.use('/api/ventas', ventaRoutes);
-  app.use('/api/eventos', eventoRoutes);
+app.use('/api/eventos', eventoRoutes);
 app.use('/api/webhook', webhookRoutes);
 
 sequelize.sync({ alter: true }).then(() => console.log('DB sincronizada'));
