89f8b7f2443e65c99cbe28a83cca0256a5bed5c2
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-22 14:49:36 -0400

    Add files via upload

---
+++ src/models/pagos.js
@@ -0,0 +1,11 @@
+// models/pagoModel.js (si algún día lo necesitas)
+module.exports = (sequelize, DataTypes) => {
+  const Pago = sequelize.define('Pago', {
+    external_reference: { type: DataTypes.STRING, allowNull: false },
+    mp_init_point: { type: DataTypes.STRING },
+    estado: { type: DataTypes.STRING, defaultValue: 'pendiente' },
+    metodo: { type: DataTypes.STRING }, // mercado_pago, transbank, etc.
+  });
+
+  return Pago;
+};
