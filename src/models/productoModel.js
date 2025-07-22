dffe81c10e92556d019634e78c5058ecdd382551
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-21 21:51:28 -0400

    Add files via upload

--- src/models/productoModel.js
+++ src/models/productoModel.js
@@ -2,26 +2,16 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Producto = sequelize.define('Producto', {
-    nombre: DataTypes.STRING,
-    descripcion: DataTypes.TEXT,
-    precio: DataTypes.FLOAT,
-    stock: DataTypes.INTEGER,
-    creado_en: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+  nombre: DataTypes.STRING,
+  descripcion: DataTypes.TEXT,
+  precio_venta: DataTypes.FLOAT,
+  precio_compra: DataTypes.FLOAT,
+  cantidad: DataTypes.INTEGER,
+  lote: DataTypes.STRING,
+  ubicacion: DataTypes.STRING,
+  fecha_compra: DataTypes.DATE,
+  id_producto: DataTypes.STRING
 });
 
-module.exports = (sequelize, DataTypes) => {
-  const Producto = sequelize.define('Producto', {
-    nombre: DataTypes.STRING,
-    descripcion: DataTypes.TEXT,
-    precio_venta: DataTypes.FLOAT,
-    precio_compra: DataTypes.FLOAT,
-    cantidad: DataTypes.INTEGER,
-    lote: DataTypes.STRING,
-    ubicacion: DataTypes.STRING,
-    fecha_compra: DataTypes.DATE,
-    id_producto: DataTypes.STRING // si lo usas
-  });
-
-  return Producto;
-};
+module.exports = Producto;
 
