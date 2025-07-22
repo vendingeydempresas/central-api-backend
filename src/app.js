0983e7bc2a3753bdfae0adbee6ebb404eb01670a
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-21 20:44:19 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const { sequelize } = require('./models');
 
 // const authRoutes = require('./routes/auth');
 // const clienteRoutes = require('./routes/clientes');
-// const productoRoutes = require('./routes/productos');
+  const productoRoutes = require('./routes/productos');
 // const ventaRoutes = require('./routes/ventas');
-// const eventoRoutes = require('./routes/eventos');
+  const eventoRoutes = require('./routes/eventos');
 const webhookRoutes = require('./routes/webhook');
 
 const app = express();
@@ -17,9 +17,9 @@ app.use(bodyParser.json());
 
 // app.use('/api/auth', authRoutes);
 // app.use('/api/clientes', clienteRoutes);
-// app.use('/api/productos', productoRoutes);
+ app.use('/api/productos', productoRoutes);
 // app.use('/api/ventas', ventaRoutes);
-// app.use('/api/eventos', eventoRoutes);
+  app.use('/api/eventos', eventoRoutes);
 app.use('/api/webhook', webhookRoutes);
 
 sequelize.sync({ alter: true }).then(() => console.log('DB sincronizada'));
