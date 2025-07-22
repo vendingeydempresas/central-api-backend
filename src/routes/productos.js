ad48c35692ef606207d4a9e2633bf6e866d8af0d
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-21 21:01:51 -0400

    Add files via upload

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,1 +1,12 @@
-// productos routes
+// routes/productos.js
+const express = require('express');
+const router = express.Router();
+const { crearProducto, obtenerProductos } = require('../controllers/productoController');
+
+// Ruta para crear producto (POST)
+router.post('/', crearProducto);
+
+// Ruta para obtener todos los productos (GET)
+router.get('/', obtenerProductos);
+
+module.exports = router;
