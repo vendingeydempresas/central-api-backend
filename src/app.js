c05d9f8922733eb7ad30551ca15b3163cb3ef41b
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-25 10:18:45 -0400

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' }); // ✅ Esto carga el .env desde un nivel arriba
+require('dotenv').config({ path: '../.env' }); // Carga el .env desde un nivel arriba
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -8,25 +8,27 @@ const { sequelize } = require('./models');
 const productoRoutes = require('./routes/productos');
 const eventoRoutes = require('./routes/eventos');
 const webhookRoutes = require('./routes/webhook');
-const pagosRoutes = require('./routes/pagos').default;
-const detallecompraRoutes = require('./routes/detallecompra'); // Nueva ruta para productos
+const pagoRoutes = require('./routes/pago'); // Correcta importación de las rutas de pago
+const detallecompraRoutes = require('./routes/detallecompra'); // Nueva ruta para detallecompra
 
+// Log de comprobación
 console.log('productoRoutes:', typeof productoRoutes);
 console.log('eventoRoutes:', typeof eventoRoutes);
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Middleware para parsear JSON
 
 // Rutas de la API
 app.use('/api/productos', productoRoutes);
 app.use('/api/eventos', eventoRoutes);
 app.use('/api/webhook', webhookRoutes);
-app.use("/api", pagosRoutes); // Quedará como /api/create-mp-link
-app.use('/api/detallecompra', detallecompraRoutes); // Nueva ruta para productos
+app.use("/api", pagoRoutes); // Quedará como /api/create-mp-link
+app.use('/api/detallecompra', detallecompraRoutes); // Ruta para manejar detalle de compra
 
 // Sincronizar la base de datos
 sequelize.sync({ alter: true }).then(() => console.log('DB sincronizada'));
 
+// Configuración del puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
