6dead407b93d90d8b0409169f10a848ed4c5c90e
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-25 11:26:10 -0400

    Add files via upload

--- src/models/index.js
+++ src/models/index.js
@@ -1,12 +1,18 @@
-const Notificacion = require('./notificacionModel');
-const sequelize = require('../config/database');
-const Cliente = require('./clienteModel');
-const Producto = require('./productoModel');
-const Venta = require('./ventaModel');
-const Evento = require('./eventoModel');
-const Usuario = require('./usuarioModel');
+import { sequelize } from '../config/database.js';  // Usa import en lugar de require
+import Notificacion from './notificacionModel.js';
+import Cliente from './clienteModel.js';
+import Producto from './productoModel.js';
+import Venta from './ventaModel.js';
+import Evento from './eventoModel.js';
+import Usuario from './usuarioModel.js';
+import Pago from './pagoModel.js'; // Incluir el modelo de Pago para la base de datos
 
+// Relaciones entre modelos
 Cliente.hasMany(Venta);
 Venta.belongsTo(Cliente);
 
-module.exports = { sequelize, Cliente, Producto, Venta, Evento, Usuario, Notificacion };
+// Otras relaciones si las tienes
+// Producto.hasMany(Venta);
+// Venta.belongsTo(Producto);
+
+export { sequelize, Cliente, Producto, Venta, Evento, Usuario, Notificacion, Pago }; // Usa export en lugar de module.exports
