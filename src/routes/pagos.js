88d5fa621039cd95bb12f72ede522b16747be9b8
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-25 10:43:49 -0400

    Add files via upload

--- src/routes/pagos.js
+++ src/routes/pagos.js
@@ -1,14 +1,13 @@
 // routes/pagos.js
 import express from "express";
-import { crearLinkMercadoPago, crearLinkTransbank } from "../controllers/pagoController.js"; // Importamos ambas funciones
+import { crearLinkMercadoPagoController, crearTransaccionTransbankController } from "../controllers/pagoController.js"; // Importar las funciones correctas
 
 const router = express.Router();
 
 // Ruta para crear el link de pago de Mercado Pago
-router.post("/create-mp-link", crearLinkMercadoPago);
+router.post("/create-mp-link", crearLinkMercadoPagoController);
 
 // Ruta para crear el link de pago de Webpay (Transbank)
-router.post("/create-tbk-link", crearLinkTransbank);
+router.post("/create-tbk-link", crearTransaccionTransbankController);
 
 export default router;
-
