8606735e75027e565c833bdfc8caed5f7334ecff
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-28 17:51:36 -0400

    Update detallecompraController.js

--- src/controllers/detallecompraController.js
+++ src/controllers/detallecompraController.js
@@ -18,6 +18,20 @@ exports.guardardetallecompra = async (req, res) => {
       Precio_Compra_IV,
     } = req.body;
 
+
+exports.obtenerDetalles = async (req, res) => {
+  try {
+    const detalles = await DetalleCompra.findAll({ order: [['createdAt', 'DESC']] });
+    res.status(200).json(detalles);
+  } catch (error) {
+    console.error('Error al obtener los detalles de compra:', error);
+    res.status(500).json({ error: 'Error al obtener los detalles de compra' });
+  }
+};
+
+
+
+
     // Validación básica
     if (!ID_Producto_IV || !Nombre_Producto_IV || !Cantidad_Link_Pago_IV) {
       return res.status(400).json({ error: 'Faltan datos obligatorios' });
