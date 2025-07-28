3161e9735218063789b4da46c31fd4122d1bb822
vendingeydempresas <vendingeydempresas@gmail.com>
2025-07-28 17:58:51 -0400

    Update detallecompra.js

--- src/routes/detallecompra.js
+++ src/routes/detallecompra.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const detallecompraController = require('../controllers/detallecompraController');
+const { guardardetallecompra, obtenerDetalles } = require('../controllers/detallecompraController');
 
 // Ruta POST para guardar un detalle de compra
-router.post('/', detallecompraController.guardardetallecompra);
+router.post('/', guardardetallecompra);
+
+// Ruta GET para obtener todos los detalles guardados
 router.get('/', obtenerDetalles);
+
 module.exports = router;
