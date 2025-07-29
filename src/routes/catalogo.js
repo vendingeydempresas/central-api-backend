09505155ec5bee34c2d222eb96b965a5b616bbaa
DarioIgnacio20 <darioignacio20@gmail.com>
2025-07-29 12:34:21 -0400

    Modificando ruta catalogo para recibir imaganes y videos

--- src/routes/catalogo.js
+++ src/routes/catalogo.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 const { crearProductoCatalogo, obtenerCatalogo } = require('../controllers/catalogoController');
 
-router.post('/', crearProductoCatalogo);
+// Importa el middleware de multer
+const upload = require('../middleware/multer');
+
+// Ruta para crear producto con imágenes y video
+router.post(
+  '/',
+  upload.fields([
+    { name: 'imagenPrincipal', maxCount: 1 },
+    { name: 'imagenesAdicionales', maxCount: 4 },
+    { name: 'video', maxCount: 1 }
+  ]),
+  crearProductoCatalogo
+);
+
+// Ruta para obtener el catálogo
 router.get('/', obtenerCatalogo);
 
 module.exports = router;
